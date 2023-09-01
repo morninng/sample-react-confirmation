a42becb6e45687286845ee8eafa6ecbf56c94b50
morninng <mr_morninng_star_dash@yahoo.co.jp>
2023-09-01 17:49:07 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         hhh
-        {/* <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -21,7 +21,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
+        </a> 
       </header>
       <div>
         sss
--- src/counter/Counter.tsx
+++ src/counter/Counter.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
+// import axios from "axios";
 import { useAppSelector, useAppDispatch } from '../hooks'
 
 import { decrement, increment } from './counterSlice'
@@ -8,6 +8,12 @@ export function Counter() {
   // The `state` arg is correctly typed as `RootState` already
   const count = useAppSelector((state) => state.counter.value)
   const dispatch = useAppDispatch()
+
+  // useEffect(() => {
+  //   axios.get('https://recording.mixidea.org/').then((response) => {
+  //     console.log(response)
+  //   });
+  // }, []);
 
   // omit rendering logic
   return (
